8da3de4dc66ac3d51adeaee52fa9faa62d119ce7
art84parthu <art84parthu@gmail.com>
2015-03-27 16:33:17 +0530

    Using Java's own set implementation

--- src/challenges/medium/LongestLine.java
+++ src/challenges/medium/LongestLine.java
@@ -1,9 +1,10 @@
 package challenges.medium;
 
 import java.io.*;
+import java.util.*;
 
 public class LongestLine{
-	public static void main(String[] args){
+	public static void main2(String[] args){
 		 try {
 		        File file = new File(args[0]);
 		        BufferedReader in = new BufferedReader(new FileReader(file));
@@ -35,6 +36,27 @@ public class LongestLine{
 		    }     
 	}
 	
+	public static void main(String[] args){
+		 try {
+		        File file = new File(args[0]);
+		        BufferedReader in = new BufferedReader(new FileReader(file));
+		        String line = in.readLine();
+		        
+		        int limit = Integer.parseInt(line);
+		        LengthComparator comparator = new LengthComparator();
+		        TreeSet<String> sortedLines = new TreeSet<String>(comparator);
+		        while ((line = in.readLine()) != null) {
+		        	sortedLines.add(line);
+		        }
+		        for(int i=0; i<limit; i++){
+		        	System.out.println(sortedLines.pollFirst());
+		        }
+		        in.close();
+		    } catch (IOException e) {
+		        System.out.println("File Read Error: " + e.getMessage());
+		    }     
+	}
+	
 	private static void pushLinesDown(int i, String newLine, String[] lines){
 		if(i >= lines.length) return;
 		if(lines[i].equals("")){
@@ -50,6 +72,16 @@ public class LongestLine{
 	private static void printArray(String[] output){
 		for(String str: output){
 			System.out.println(str);
+		}
+	}
+	
+	private static class LengthComparator implements Comparator<String>{
+		public int compare(String s1, String s2){
+			if(s1.length() < s2.length()){
+				return 1;
+			}else{
+				return -1;
+			}
 		}
 	}
 }
