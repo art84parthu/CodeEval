1cc8b4a7bb8da2b53fc394dd528b7f4612995d2a
Arthy Vijayaraghavan <art84parthu@gmail.com>
2015-04-03 23:50:03 +0530

    Updated package and class name

--- src/challenges/medium/CheckEndianness.java
+++ src/challenges/medium/CheckEndianness.java
@@ -1,6 +1,8 @@
+package challenges.medium;
+
 import java.nio.ByteOrder;
 
-public class CheckEndianess{
+public class CheckEndianness{
 	public static void main(String[] args){
 		if(ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN){
 			System.out.println("BigEndian");
