0fd36b7240aaf48a29eedc8411e9a6f1eaced815
Arthy Vijayaraghavan <art84parthu@gmail.com>
2015-04-03 11:56:18 +0530

    Endianness problem solution - 100% Codeeval

---
+++ src/challenges/medium/CheckEndianness.java
@@ -0,0 +1,14 @@
+import java.nio.ByteOrder;
+
+public class CheckEndianess{
+	public static void main(String[] args){
+		if(ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN){
+			System.out.println("BigEndian");
+		}else if(ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN){
+			System.out.println("LittleEndian");
+		}else{
+		  //This will never happen actually.
+			System.out.println("Unknown");
+		}
+	}
+}
